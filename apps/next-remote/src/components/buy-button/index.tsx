161f8de1c38896655185e33594d14b4d29a57ca9
Phat Pham <phpham@strongtie.com>
2024-01-04 13:03:15 +0700

    .

--- apps/next-remote/src/components/buy-button/index.tsx
+++ apps/next-remote/src/components/buy-button/index.tsx
@@ -4,7 +4,7 @@ import { HTMLAttributes } from 'react';
 export interface BuyButtonProps extends HTMLAttributes<HTMLButtonElement> {}
 
 export function BuyButton({ children, ...props }: BuyButtonProps) {
-  return <button {...props}>{children || `Buy now!`}</button>;
+  return <button {...props}>{children || `Buy now! from Next Remote`}</button>;
 }
 
 export default BuyButton;
