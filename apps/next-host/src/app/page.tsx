bfa2e9d014e19c6c525f1fa437be670270617e0d
Phat Pham <phpham@strongtie.com>
2024-01-04 13:01:00 +0700

    feat: next remote

--- apps/next-host/src/app/page.tsx
+++ apps/next-host/src/app/page.tsx
@@ -2,6 +2,7 @@
 
 // import dynamic from 'next/dynamic';
 import NxWelcome from '../components/nx-welcome';
+// eslint-disable-next-line @nx/enforce-module-boundaries
 import BuyButton from '@nextRemote/src/components/buy-button';
 
 // const BuyButton = dynamic(() => import('nextRemote'), {
